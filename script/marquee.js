c40356372837fa67b0f2d9795a85168538c5b5de
s0rest8 <dyha081@mail.ru>
2024-09-29 13:45:49 +0300

    path changed

--- script/marquee.js
+++ script/marquee.js
@@ -5,7 +5,7 @@ export default function marqueeinit() {
     let offset = 0
     let marqueeSlider = document.querySelector('.greeting-marquee__slider')
 
-    document.querySelector('.greeting-btn_next').addEventListener('click', function () {
+    document.querySelector('.greeting-slider-btn__next').addEventListener('click', function () {
         offset = offset + 167
         if (offset > 668) {
             offset = 0
@@ -13,7 +13,7 @@ export default function marqueeinit() {
         marqueeSlider.style.left = -offset + 'px'
     })
 
-    document.querySelector('.greeting-btn_prev').addEventListener('click', function () {
+    document.querySelector('.greeting-btn__prev').addEventListener('click', function () {
         offset = offset - 167
         if (offset < 0) {
             offset = 668
